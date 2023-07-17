b884d5e19ae5565cd16498e0c1ffce94095ef3c0
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-17 19:29:31 +0300

    Create a table named animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
-);
+/* Create a table named animals */
+CREATE TABLE animals(
+    id INT PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts INT, 
+    neutered boolean NOT NULL, 
+    weight_kg float NOT NULL);
