2421f0d8b8a8004774e99a7fdf2e6904954ca7d9
Habtamu (ቤናዊ)  Alemayehu <habtamualemayehu16@gmail.com>
2023-07-17 16:41:50 +0300

    Initial commit

--- schema.sql
+++ schema.sql
@@ -1,10 +1,5 @@
 /* Database schema to keep the structure of entire database. */
-/*Create sequence and  a table named animals */
-CREATE SEQUENCE table_id_seq;
-CREATE TABLE animals(id INTEGER NOT NULL DEFAULT nextval('table_id_seq'),
- name varchar(100) NOT NULL,
- date_of_birth date NOT NULL, 
- escape_attempts INT, 
- neutered boolean NOT NULL,
- weight_kg float NOT NULL, 
- PRIMARY KEY(id));
+
+CREATE TABLE animals (
+    name varchar(100)
+);
