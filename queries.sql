9fd1649c9bdaa7ca2890dcd31fb033a5415944c1
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-17 20:13:57 +0300

    List the date of birth of all animals named either 'Agumon' or 'Pikachu'.

--- queries.sql
+++ queries.sql
@@ -8,4 +8,5 @@ SELECT * FROM animals WHERE date_of_birth BETWEEN '20160101' AND '20190101';
 /* List the name of all animals that are neutered and have less than 3 escape attempts. */
 SELECT name FROM animals WHERE neutered IS true AND escape_attempts < 3;
 
-/
+/* List the date of birth of all animals named either "Agumon" or "Pikachu". */
+SELECT date_of_birth as "Date of Birth" FROM animals WHERE name IN ('Agumon', 'Pikachu');
