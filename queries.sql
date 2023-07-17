1ee1945911a40b17cdda3c6c76116de0a83d8569
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-17 20:10:38 +0300

    List the name of all animals born between 2016 and 2019.

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 
 /* Find all animals whose name ends in "mon". */
 SELECT * FROM animals WHERE name LIKE '%mon';
+/* List the name of all animals born between 2016 and 2019. */
+SELECT * FROM animals WHERE date_of_birth BETWEEN '20160101' AND '20190101';
