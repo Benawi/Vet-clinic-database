e5ee6b3d2128f49079725fb7278c10c31b0b1e7c
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-17 20:09:38 +0300

    Find all animals whose name ends in 'mon'

--- queries.sql
+++ queries.sql
@@ -1,3 +1,3 @@
 
-/* Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg) */
-SELECT *  FROM animals WHERE weigth_kg BETWEEN 10.4 and 17.3;
+/* Find all animals whose name ends in "mon". */
+SELECT * FROM animals WHERE name LIKE '%mon';
