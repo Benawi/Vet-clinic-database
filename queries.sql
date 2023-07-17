7dbb9f78211d57654b80bd23458d19b0d6a7950d
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-17 20:13:21 +0300

    List the name of all animals that are neutered and have less than 3 escape attempts.

--- queries.sql
+++ queries.sql
@@ -1,5 +1,11 @@
 
 /* Find all animals whose name ends in "mon". */
 SELECT * FROM animals WHERE name LIKE '%mon';
+
 /* List the name of all animals born between 2016 and 2019. */
 SELECT * FROM animals WHERE date_of_birth BETWEEN '20160101' AND '20190101';
+
+/* List the name of all animals that are neutered and have less than 3 escape attempts. */
+SELECT name FROM animals WHERE neutered IS true AND escape_attempts < 3;
+
+/
