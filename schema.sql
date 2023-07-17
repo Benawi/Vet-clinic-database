89a9309b488602ccad7d3a61d544ad5cae3fbac9
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-17 20:03:18 +0300

    Create a sequence and table named animals

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-/* Create a table named animals */
-CREATE TABLE animals(
-    id INT PRIMARY KEY,
-    name varchar(100) NOT NULL,
-    date_of_birth date NOT NULL,
-    escape_attempts INT, 
-    neutered boolean NOT NULL, 
-    weight_kg float NOT NULL);
+/*Create sequence and  a table named animals */
+CREATE SEQUENCE table_id_seq;
+CREATE TABLE animals(id INTEGER NOT NULL DEFAULT nextval('table_id_seq'),
+ name varchar(100) NOT NULL,
+ date_of_birth date NOT NULL, 
+ escape_attempts INT, 
+ neutered boolean NOT NULL,
+ weight_kg float NOT NULL, 
+ PRIMARY KEY(id));
