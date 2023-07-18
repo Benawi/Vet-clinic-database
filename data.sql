cc228b6db658727e69e7724c595c9c9b54e90b74
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-18 22:25:59 +0300

    Modify inserted animals so it includes the species_id

--- data.sql
+++ data.sql
@@ -21,3 +21,25 @@ INSERT INTO animals VALUES
 (default, 'Blossom', '19981013', 3, true, 17 ),
 /*  Animal: His name is Ditto. He was born on May 14th, 2022, and currently weighs 22kg. He is neutered and he has tried to escape 4 times.*/
 (default, 'Ditto', '20220614', 4, true, 22 );
+
+/* Insert the following data into the owners  */
+INSERT INTO owners(full_name, age) VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+/* Insert the following data into the species */
+INSERT INTO species(name) VALUES ('Pokemon'),('Digimon');
+
+/* Modify inserted animals so it includes the species_id value:
+If the name ends in "mon" it will be Digimon */
+BEGIN;
+UPDATE animals
+SET species_id = (
+  SELECT id FROM species
+  WHERE name IN ('%mon1%', '%mon2%')  
+)
+WHERE name LIKE '%mon';
+COMMIT;
