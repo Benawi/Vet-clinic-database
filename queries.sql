18f4ad7b0a203569db9ba96deed840eb17a2fbad
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-18 15:08:25 +0300

    Update readme file

--- queries.sql
+++ queries.sql
@@ -23,7 +23,9 @@ SELECT *  FROM animals WHERE name NOT IN ('Gabumon');
 
 /* Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg) */
 SELECT *  FROM animals WHERE weigth_kg BETWEEN 10.4 and 17.3;
-
+/* =======================================================
+   ||Vet clinic database project: query and update animals table||
+   ======================================================= */
 /* Inside a transaction update the animals table by setting the species column to unspecified */
 BEGIN;
 UPDATE animals
