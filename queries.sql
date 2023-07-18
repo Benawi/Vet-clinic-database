a7a0108c3c6431b50d7688272f45153b1927bf36
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-18 12:49:53 +0300

    Inside a transaction update the animals table by setting the species column to unspecified

--- queries.sql
+++ queries.sql
@@ -23,3 +23,9 @@ SELECT *  FROM animals WHERE name NOT IN ('Gabumon');
 
 /* Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg) */
 SELECT *  FROM animals WHERE weigth_kg BETWEEN 10.4 and 17.3;
+
+/* Inside a transaction update the animals table by setting the species column to unspecified */
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
