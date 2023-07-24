7b46e0c4204c40b14f1f0be2b3c3aadd1dff61ca
wolterbwambale <benplaan1@gmail.com>
2023-07-24 21:58:48 +0300

    update the data.sql with email insert

--- data.sql
+++ data.sql
@@ -228,3 +228,9 @@ INSERT INTO visits(animal_id, vet_id, date_of_visit) VALUES
   '20210111'
 );
 COMMIT;
+
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+Insert into owners 
+(full_name, email) select 'Owner ' || generate_series(1,2500000),
+ 'owner_' || generate_series(1,2500000) || '@mail.com';
